2bded0cfe6f565f0685ea3a4c24a2e9e63a43f8f
Jdavidrb <u.jdavidrb@gmail.com>
2025-01-22 18:10:26 -0500

    init hw 1 agra

---
+++ tasks2/1/factorization.cpp
@@ -0,0 +1,23 @@
+/* 
+Jose David Ruano Burbano (8982982)
+Arboles y grafos 2025-2
+
+Analisis de la complejidad:
+
+*/
+#include <cstdio>
+
+int main(){
+    
+    int n;
+    scanf("%d", &n);
+    while (n != 0) {
+        scanf("%d", &n);
+
+    }
+
+    
+
+
+    return 0;
+}
